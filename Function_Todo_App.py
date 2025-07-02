dfd7f294b7ab51bc681f615f386b70d68e48190f
suhas <suhasreddy.ir@gmail.com>
2025-07-02 22:31:56 +1000

    Creating File Structure

--- Function_Todo_App.py
+++ Function_Todo_App.py
@@ -1,8 +1,22 @@
 
+FP = "Todos.txt"
 
 #Add First Function Here
-
+"""
+The Goal of this get_todos function is to get the todos list from a file
+"""
+def get_todos(File_Path = FP):
+    with open(File_Path, 'r') as f:
+        todos_read = f.readlines()
+    return todos_read
 
 
 #Add Second Function Here
+"""
+The Goal of this write_todos function is to write the todos list from a file
+"""
+def write_todos(todos_list, File_Path = FP):
+    with open(File_Path, 'w') as f:
+        f.writelines(todos_list)
+
 
