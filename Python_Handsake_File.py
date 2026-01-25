63b8ac44837b0169b54859ac301cde2feec17204
suhas <suhasreddy.ir@gmail.com>
2026-01-25 16:00:16 +1100

    First Checkpoint - WebApp Commit

---
+++ Python_Handsake_File.py
@@ -0,0 +1,16 @@
+#print("hello world")
+
+#to get the type of the variable
+#print(type("hello world"))
+#print(type(["a", "b", "c", "d"]))
+
+#to get input from user
+#x = input("Enter your String: ")
+#y = len(x)
+#print("The length of your string is: ",y, " characters.")
+
+#to get the list of methods available for a data type
+print(dir("df"))
+print(help("df".capitalize))
+import builtins
+print(dir(builtins))
